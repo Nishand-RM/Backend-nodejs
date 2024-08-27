7b3a1ad067cd9f98e2e766465786203f7ccf0cf7
Nishand-RM <nishandrm9@gmail.com>
2024-08-27 20:28:10 +0530

    creating an API application using NODEJS

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
-// server.mjs
-const {createServer} = require('http');
-const server = createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello nishand!\n');
+//create a server
+
+const http = require('http'); //commonjs method to import
+
+//import http from 'http';//ES6 way to import 
+
+//create a server object
+const server = http.createServer((request,response)=>{
+    const{url,method} = request;
+
+    if(url === '/'){
+        //return response.end("welcome to page");
+        if(method === 'GET'){
+            return response.end("GET METHOD");
+
+        }
+
+        else if(method === 'POST'){
+            return response.end("POST METHOD");
+
+        }
+
+        else if(method === 'DELETE'){
+            return response.end("DELETE METHOD");
+
+        }
+        else{
+            return response.end('no method');
+        }
+    }
+    else if(url === '/page'){
+        return response.end("this is page endpoint");
+    }
+    
+    else{
+        return response.end("url not found");
+    }
+    /*console.log(request.method, request.url,request.headers);
+    console.log(request.body);
+    response.end("hello world");//write response to client*/
+
 });
 
-// starts a simple http server locally on port 3000
-server.listen(3000, '127.0.0.1', () => {
-  console.log('Listening on 127.0.0.1:3000');
+//start the server listening for request
+server.listen(3001,'localhost',()=>{
+    console.log("server is running on http://localhost:3001");
 });
-
-// run with `node server.mjs`
