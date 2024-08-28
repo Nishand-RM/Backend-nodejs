a21220f098ce32ee4fea400c5cd6621bbc29dd16
Nishand-RM <nishandrm9@gmail.com>
2024-08-28 12:49:23 +0530

    file manupulation

--- index.js
+++ index.js
@@ -5,30 +5,79 @@ const express = require('express');
 //create an express application
 const app = express();
 
-//define the routes and their corresponding functions
-app.get('/',(req,res)=>{
-    res.send("GET METHOD!");
+//to access the files
+const fs = require('fs');
+
+//to get the file info
+app.get('/info',(req,res)=>{
+    fs.stat('./files/file.txt', (err,stats)=>{
+        if(err){
+            res.send(err);
+        }
+        else{
+            //to get only particular info about file
+           /* res.json({
+                size: `${stats.size} bytes`,
+                isFile: stats.isFile(),
+                isDirectory: stats.isDirectory(),
+            });*/
+
+            //to get entire info about file
+            res.send(stats);
+        }
+    })
 });
 
-app.put('/',(req,res)=>{
-    res.send("PUT METHOD!");
+//to write a new context in a file
+
+app.post('/write', (req,res)=>{
+    fs.writeFile('./files/file.txt','How are you guys',(err)=>{
+        if(err){
+            res.send(err);
+        }
+        else{
+            res.send('File created successfully');
+        }
+    })
 });
 
-app.post('/',(req,res)=>{
-    res.send("POST METHOD!");
+//to create a new file and write in that
+app.post('/create', (req,res)=>{
+    fs.writeFile('./files/newfile.txt','nishand here!',(err)=>{
+        if(err){
+            res.send(err);
+        }
+        else{
+            res.send('File created successfully');
+        }
+    })
 });
 
 
-app.delete('/',(req,res)=>{
-    res.send("DELETE METHOD!");
-});
+//to read a file
 
+app.get('/read',(req,res)=>{
+    fs.readFile('./files/newfile.txt','utf8',(err,data)=>{
+        if(err){
+            res.send(err);
+        }
+        else{
+            res.send(data);
+        }
+    })
+})
 
-//methods with endpoints
-app.get('/test',(req,res)=>{
-    res.send("GET METHOD in test endpoint!");
-});
-
+//to copy from one file to another
+app.get('/copy',(req,res)=>{
+    fs.copyFile('./files/fileA.txt','./files/fileB.txt',(err)=>{
+        if(err){
+            res.send(err);
+        }
+        else{
+            res.send('copied successfully');
+        }
+    })
+})
 
 
 //start the server by listening on port for incoming requests
