aa802ab9e32bb1fbd8e5533c10374d1187fbc82b
Nishand-RM <nishandrm9@gmail.com>
2024-09-12 21:34:47 +0530

    no use

--- index.js
+++ index.js
@@ -1,32 +1,60 @@
+const companies = [
+    {
+        id: 1,
+        name: "Google",
+        location: "Mountain View, California",
+        email: "careers@google.com",
+        phone: "650-253-0000",
+        website: "https://careers.google.com",
+        createdAt: "2021-09-01T00:00:00Z",
+        updatedAt: "2021-09-01T00:00:00Z"
+    },
+    {
+        id: 2,
+        name: "Facebook",
+        location: "Menlo Park, California",
+        email: "careers@facebook.com",
+        phone: "650-543-4800",
+        website: "https://www.facebook.com/careers",
+        createdAt: "2021-09-01T00:00:00Z",
+        updatedAt: "2021-09-01T00:00:00Z"
+    },
+    {
+        id: 3,
+        name: "Amazon",
+        location: "Seattle, Washington",
+        email: "careers@amazon.com",
+        phone: "206-266-1000",
+        website: "https://www.amazon.jobs",
+        createdAt: "2021-09-01T00:00:00Z",
+        updatedAt: "2021-09-01T00:00:00Z"
+    },
+];
+
+
 
 const express = require('express');
 
 const app = express();
 
-const fs = require('fs');
+app.use(express.json());
 
 
-
-const now = new Date().toString();
-
-
-app.post('/create', (req, res) => {
-    fs.writeFile('./files/date-time.txt', now, (err) => {
-        if (err) {
-            res.send(err);
-        }
-        else {
-
-            res.send("successfull");
-        }
-    })
+// to get the companies info
+app.get('/companies',(req,res)=>{
+    res.send(companies);
 })
 
-app.get('/path',(req,res)=>{
-   
-    const file = fs.readdirSync('./files');
- 
-    res.send(file);
+//to update the new company in the array
+app.post('/companies',(req,res)=>{
+    const company = req.body;
+    company.id = companies[companies.length - 1].id + 1;
+    company.createdAt = new Date().toISOString();
+    company.updatedAt = new Date().toISOString();
+
+    companies.push(company);
+
+    res.json({message: "company created successfully"});
 
 })
 
